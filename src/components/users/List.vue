80d244a44e4be033587fc16e0968da3a16193a5c
David Ang <davidang09@gmail.com>
2018-05-26 20:57:20 +0800

    mock api data for the bikers table listing

--- src/components/users/List.vue
+++ src/components/users/List.vue
@@ -10,8 +10,18 @@
         <th>Ride in group</th> 
         <th>Days of the week</th> 
         <th>Registration day</th>
+        <th></th>
       </thead>
       <tbody>
+        <tr v-for="biker in bikers" :key="biker.id">
+          <td>{{biker.name}}</td>
+          <td>{{biker.email}}</td>
+          <td>{{biker.city}}</td>
+          <td>{{biker.rideInGroup}}</td>
+          <td>{{biker.daysOfTheWeek}}</td>
+          <td>{{biker.registration}}</td>
+          <td></td>
+        </tr>
       </tbody>
     </table>
     </div>
@@ -19,8 +29,14 @@
 </template>
 
 <script>
-export default {
+import bikersData from '../../api/bikers.json'
 
+export default {
+  data() {
+    return {
+      bikers: bikersData.bikers,
+    } 
+  }
 }
 </script>
 
